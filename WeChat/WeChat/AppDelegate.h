f2e5ce0c85973f1015380688909f9d2db4455c81
YangXiong <315581365@qq.com>
2016-11-05 17:21:27 +0800

    initWeChatPrject

---
+++ WeChat/WeChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeChat
+//
+//  Created by tarena15 on 16/11/5.
+//  Copyright © 2016年 tarena15. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeChat/WeChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeChat
+//
+//  Created by tarena15 on 16/11/5.
+//  Copyright © 2016年 tarena15. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
